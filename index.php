6ee7d122f56ecb7f65ec51f30721b81b6a26fd1c
Dacaspex <Caspersmits11@outlook.com>
2015-11-12 15:46:14 +0100

    Upload everything so far to Github

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+	<head>
+		<title>E D E N</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<link href='https://fonts.googleapis.com/css?family=Roboto:100,300' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div id="page-top">
+			<ul id="navbar">
+				<li><a href="#" id="active">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Blog</a></li>
+			</ul>
+		</div>
+		<div id="hero-unit">
+			<div id="inner-wrapper">
+				<div id="message-box">
+					<span id="name">E D E N</span><br />
+					<span id="qoute">Music for you</span>
+				</div>
+			</div>
+		</div>
+		<div id="wrapper">
+			<div class="panel panel-info">Failed to login</div>
+			<div id="col-1">
+				<form id="newsletter-box">
+					<span>Sign in for the newsletter</span>
+					<input type="text" />
+					<input class="button button-success button-inline" type="submit" value="Send">
+				</form>
+			</div>
+			<div id="col-2">
+				<div class="header">
+					Welcome
+				</div>
+				<p>
+					Jonathan Ng, going by the name of <strong>EDEN</strong> is a Electro, House and Melodic Dubstep producer, originating in Hong Kong SAR but now lives in Dublin, Ireland. EDEN so far has two releases on Monstercat which are 'Scribble' and 'The Fire' which are both in collaboration with Puppet. Since then, The Eden Project has mentioned on social media that he would love to be part of the Monstercat family, however wants to remain with his current music label, NoCopyRightSounds for a while longer.
+				</p>
+			</div>
+		</div>
+	</body>
+</html>
